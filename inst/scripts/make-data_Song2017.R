a12205ac8e05dc4671f40ba54e21a8b89ef34c29
Federico Marini <marinif@uni-mainz.de>
2021-04-22 21:07:42 +0200

    polish script to create data itself and annotate samples

--- inst/scripts/make-data_Song2017.R
+++ inst/scripts/make-data_Song2017.R
@@ -19,16 +19,60 @@ library(SummarizedExperiment)
 
 # preparing something more on the colData to have the information available directly
 coldata_Song2017 <- DataFrame(
-  srr_id = colnames(tpmdata)
+  sra_id = colnames(tpmdata)
 )
 
-# coldata_Song2017$group ...
+# would be used like...
+ti_quant <- quantiseqr::run_quantiseq(
+  expression_data = tpmdata,
+  signature_matrix = "TIL10",
+  is_arraydata = FALSE,
+  is_tumordata = TRUE,
+  scale_mRNA = TRUE
+)
+
+
+# annotating the samples
+library("GEOquery")
+gds <- getGEO("GSE75299")
+pdata <- pData(gds[[1]])
+
+# from the run table, also made available in quantiseqr directly
+sraInfo <- read.csv(
+  system.file("extdata", "SRP066571runtable.txt", package = "quantiseqr"),
+  header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+coldata_Song2017$geo_accession <-
+  sraInfo[, "SampleName"][match(coldata_Song2017$sra_id, sraInfo$Run)]
+
+# keeping the GEO info only for matched patient datasets
+dim(pdata)
+
+is_patient <- grep("^Pt", pdata[, "title"])
+
+coldata_Song2017_full <- merge(
+  x = coldata_Song2017,
+  y = pdata,
+  by = "geo_accession"
+)
+
+coldata_Song2017_full$is_patient <- grepl("^Pt", coldata_Song2017_full[, "title"])
+
 
 Song2017_MAPKi_treatment <- SummarizedExperiment(
   assays = SimpleList(
     abundance = tpmdata
   ),
-  colData = coldata_Song2017
+  colData = coldata_Song2017_full
 )
 
 save(Song2017_MAPKi_treatment, file = "Song2017_MAPKi_treatment.Rda")
+
+# so that one can use it in quantiseqr with...
+Song2017_MAPKi_treatment_tiquant <- quantiseqr::run_quantiseq(
+  expression_data = Song2017_MAPKi_treatment,
+  signature_matrix = "TIL10",
+  is_arraydata = FALSE,
+  is_tumordata = TRUE,
+  scale_mRNA = TRUE
+)
